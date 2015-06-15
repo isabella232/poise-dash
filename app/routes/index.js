e1740595ec876826d2b10b7dbb00232a0214aacc
Noah Kantrowitz <noah@coderanger.net>
2015-06-15 14:56:17 -0700

    Allow overriding the backend service URL.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
+import config from 'poise-dash/config/environment';
 
 export default Ember.Route.extend({
   model: function() {
-    return Ember.$.getJSON('https://poise-dash-data.herokuapp.com/').then(function(data) {
+    var url = config.APP.dataUrl || 'poise-dash-data';
+    if(url.indexOf('://') === -1) {
+      url = 'https://' + url + '.herokuapp.com/';
+    }
+    return Ember.$.getJSON(url).then(function(data) {
       return Object.keys(data).map(function(key) {
         var value = data[key];
         value.id = key;
